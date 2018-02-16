dc3e0f851101bc1e2eaae17f113667912fa1487f
redmage17 <raistlen888@gmail.com>
2018-02-16 16:42:13 -0500

    Clean up imports added proper fluid

--- src/main/java/GTRedtech/core/loaders/postload/GTR_MachineRecipeLoader.java
+++ src/main/java/GTRedtech/core/loaders/postload/GTR_MachineRecipeLoader.java
@@ -2,22 +2,20 @@ package GTRedtech.core.loaders.postload;
 
 
 
-import GTRedtech.api.util.GTR_RecipeAdder;
+import GTRedtech.GTRedtech;
+
 
 import GTRedtech.core.lib.CORE;
-import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
-import gregtech.api.enums.Materials;
-
-import gregtech.api.enums.OrePrefixes;
-import gregtech.api.util.GT_OreDictUnificator;
 import net.minecraft.item.ItemStack;
 
 
 public class GTR_MachineRecipeLoader  {
     public static   void run() {
 
-        Object o = new Object[0];
-        CORE.RA.addEmbryonicGrowthChamberRecipe(new ItemStack[]{ItemList.Circuit_Chip_Stemcell.get(1,o)},Materials.Water.getFluid(1000L), ItemList.Circuit_Chip_Stemcell.get(1,o), 400, 16);
-        }
+
+
+        CORE.RA.addEmbryonicGrowthChamberRecipe(new ItemStack[]{ItemList.Circuit_Chip_Stemcell.get(1)},GTRedtech.AGrowthSolution.getFluid(1000L), ItemList.Circuit_Chip_Stemcell.get(1), 400, 16);
+
+    }
 }
